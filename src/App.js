65243982e66631c842007b9c82d017bd5a9ba3dc
Debashish D. Dev <hello@debashish.dev>
2022-09-17 23:28:52 +0600

    Nav Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" elemen={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
