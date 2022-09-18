3b66c0d1b2438f76610b5314572053a5bd78cf5b
Debashish D. Dev <hello@debashish.dev>
2022-09-18 23:19:44 +0600

    Fixed Typo

--- src/Pages/Shared/DashNav.jsx
+++ src/Pages/Shared/DashNav.jsx
@@ -5,7 +5,7 @@ const DashNav = () => {
     <div>
       <div className="navbar bg-base-100 border-b">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">FileCheckAPp</a>
+          <a className="btn btn-ghost normal-case text-xl">FileCheckApp</a>
         </div>
         <div className="flex-none gap-2">
           <div className="form-control">
