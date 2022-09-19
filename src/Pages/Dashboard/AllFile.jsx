60b0d72114c9d36c61c94b1cc41c3976551aad46
Debashish D. Dev <hello@debashish.dev>
2022-09-19 13:38:24 +0600

    Added Live API

--- src/Pages/Dashboard/AllFile.jsx
+++ src/Pages/Dashboard/AllFile.jsx
@@ -5,60 +5,54 @@ import Swal from "sweetalert2";
 import Loading from "../Shared/Loading";
 import AddFile from "./AddFile";
 
-
 const AllFile = () => {
-
-  const { isLoading, error, data:allFiles,refetch } = useQuery('allFiles', () =>
-    fetch(('http://localhost:5000/files'), {
-      method: 'GET',
+  const {
+    isLoading,
+    error,
+    data: allFiles,
+    refetch,
+  } = useQuery("allFiles", () =>
+    fetch("https://fileapppro.herokuapp.com/files", {
+      method: "GET",
       headers: {
         "content-type": "application/json",
-      }
-    }).then(res =>
-      res.json()
-    )    
-  )
-  refetch()
+      },
+    }).then((res) => res.json())
+  );
+  refetch();
 
   if (isLoading) {
-    return <Loading/>
+    return <Loading />;
   }
 
-  if (error) return 'An error has occurred: ' + error.message
-  
-  const handleDelete = id => {
+  if (error) return "An error has occurred: " + error.message;
+
+  const handleDelete = (id) => {
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "You won't be able to revert this!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
-
       if (result.value) {
-        const url = `http://localhost:5000/files/${id}`;
+        const url = `https://fileapppro.herokuapp.com/files/${id}`;
         fetch(url, {
-          method: 'DELETE'
+          method: "DELETE",
         })
-          .then(res => res.json())
-          .then(data => {
-            console.log(data)
-            const remaining = allFiles.filter(file => file._id !== id);
-          })
-
-
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            const remaining = allFiles.filter((file) => file._id !== id);
+          });
       }
       if (result.isConfirmed) {
-        Swal.fire(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        )
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
       }
-    })
-  }
+    });
+  };
   return (
     <div>
       <div className="flex justify-end mb-5">
@@ -79,25 +73,23 @@ const AllFile = () => {
           </thead>
           <tbody>
             {/* <!-- row 1 --> */}
-            {
-              allFiles.map((file, index) =>
-                <tr>
-                  <th>{index + 1}</th>
-                  <td>{file.title}</td>
-                  <td>{file.model}</td>
-                  <td>{file.year}</td>
-                  <td>Pending</td>
-                  <td className="flex gap-1">
-                    <span>
-                      <BsPencilSquare />
-                    </span>
-                    <span>
-                      <BsTrash onClick={() => handleDelete(file._id)} />
-                    </span>
-                  </td>
-                </tr>
-              )
-            }
+            {allFiles.map((file, index) => (
+              <tr>
+                <th>{index + 1}</th>
+                <td>{file.title}</td>
+                <td>{file.model}</td>
+                <td>{file.year}</td>
+                <td>Pending</td>
+                <td className="flex gap-1">
+                  <span>
+                    <BsPencilSquare />
+                  </span>
+                  <span>
+                    <BsTrash onClick={() => handleDelete(file._id)} />
+                  </span>
+                </td>
+              </tr>
+            ))}
             {/* <!-- row 2 --> */}
           </tbody>
         </table>
--- src/Pages/Dashboard/File.jsx
+++ src/Pages/Dashboard/File.jsx
@@ -1,61 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import AllFile from './AllFile';
+import React, { useEffect, useState } from "react";
+import AllFile from "./AllFile";
 import { useQuery } from "react-query";
-import Loading from '../Shared/Loading';
+import Loading from "../Shared/Loading";
 
 const File = () => {
-    const { isLoading, error, data:allFiles,refetch } = useQuery('allFiles', () =>
-    fetch(('http://localhost:5000/files'), {
-      method: 'GET',
+  const {
+    isLoading,
+    error,
+    data: allFiles,
+    refetch,
+  } = useQuery("allFiles", () =>
+    fetch("https://fileapppro.herokuapp.com/files", {
+      method: "GET",
       headers: {
         "content-type": "application/json",
-      }
-    }).then(res =>
-      res.json()
-    )    
-  )
-  refetch()
+      },
+    }).then((res) => res.json())
+  );
+  refetch();
 
   if (isLoading) {
-    return <Loading/>
+    return <Loading />;
   }
 
-  if (error) return 'An error has occurred: ' + error.message
-    return (
-        <div>
-            <div className='grid gap-10 grid-cols-2 lg:grid-cols-4 mr-10 p-20'>
-                <div class="card w-full h-32 bg-orange-600 shadow-xl">
-                    <div class="card-body text-center text-white">
-                        <h2 class="text-2xl font-bold">{allFiles.length}</h2>
-                        <p>Total files</p>
-                    </div>
-                </div>
-
-                <div class="card w-full h-32 bg-lime-600 shadow-xl">
-                    <div class="card-body text-center text-white">
-                        <h2 class="text-2xl font-bold">9</h2>
-                        <p>Closed Bugs</p>
-
-                    </div>
-                </div>
-
-                <div class="card w-full h-32 bg-green-600 shadow-xl">
-                    <div class="card-body text-center text-white">
-                        <h2 class="text-2xl font-bold">12</h2>
-                        <p>Overdue</p>
-                    </div>
-                </div>
-
-                <div class="card w-full h-32 bg-teal-600 shadow-xl">
-                    <div class="card-body text-center text-white">
-                        <h2 class="text-2xl font-bold">8</h2>
-                        <p>Due in 7 days</p>
-                    </div>
-                </div>
-            </div>
-            <AllFile/>
+  if (error) return "An error has occurred: " + error.message;
+  return (
+    <div>
+      <div className="grid gap-10 grid-cols-2 lg:grid-cols-4 mr-10 p-20">
+        <div class="card w-full h-32 bg-orange-600 shadow-xl">
+          <div class="card-body text-center text-white">
+            <h2 class="text-2xl font-bold">{allFiles.length}</h2>
+            <p>Total files</p>
+          </div>
         </div>
-    );
+
+        <div class="card w-full h-32 bg-lime-600 shadow-xl">
+          <div class="card-body text-center text-white">
+            <h2 class="text-2xl font-bold">9</h2>
+            <p>Closed Bugs</p>
+          </div>
+        </div>
+
+        <div class="card w-full h-32 bg-green-600 shadow-xl">
+          <div class="card-body text-center text-white">
+            <h2 class="text-2xl font-bold">12</h2>
+            <p>Overdue</p>
+          </div>
+        </div>
+
+        <div class="card w-full h-32 bg-teal-600 shadow-xl">
+          <div class="card-body text-center text-white">
+            <h2 class="text-2xl font-bold">8</h2>
+            <p>Due in 7 days</p>
+          </div>
+        </div>
+      </div>
+      <AllFile />
+    </div>
+  );
 };
 
 export default File;
